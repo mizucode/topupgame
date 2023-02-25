4e4ed03c8b286b52b716e6a2ff5939b8c3f82977
mizucode <mizurisysthensis3@gmail.com>
2023-02-25 14:47:35 +0700

    sliderres

--- script.js
+++ script.js
@@ -19,7 +19,37 @@ var swiper = new Swiper(".mySwiper", {
 
     breakpoints: {
         0: {
+            slidesPerView: 3,
+        },
+        800: {
+            slidesPerView: 5,
+        },
+        1920: {
             slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".mySwiperr", {
+    slidesPerView: "auto",
+    spaceBetween: 1,
+    loop: false,
+    loopedSlides: 10,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#more-bck2",
+        prevEl: "#more2",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 3,
         },
         800: {
             slidesPerView: 5,
